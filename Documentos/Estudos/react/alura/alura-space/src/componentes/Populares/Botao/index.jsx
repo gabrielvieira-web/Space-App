820c6f8ffdf728eee8c9636e290cc7b8006f7cdf
gabrielvieira-web <125363278+gabrielvieira-web@users.noreply.github.com>
2023-09-08 12:42:13 -0300

    Delete Documentos/Estudos/react/alura/alura-space directory

--- Documentos/Estudos/react/alura/alura-space/src/componentes/Populares/Botao/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './Botao.module.scss'
-
-export default function Botao({children}) {
-  return (
-    <button className={styles.botao}>{children}</button>
-  )
-}
